3bc6ea78d5b9c16ffddcd3eafdfc94c50984d0b1
Leoo05 <jaimeleonxd@gmail.com>
2024-05-14 18:07:06 -0500

    added validation to grade creation and update

--- internal/application/usecases/grade/grade.usecase.go
+++ internal/application/usecases/grade/grade.usecase.go
@@ -18,6 +18,9 @@ func NewGradeCase(gradeRepository repository.IGradesRepository) *GradeUseCase {
 }
 
 func (usecase *GradeUseCase) PostGrade(grade *entities.Grade) error {
+	if grade.Grade < 0 || grade.Grade > 5 {
+		return fmt.Errorf("error invalid Grade")
+	}
 	err := usecase.GradeRepository.CreateGrade(grade)
 	if err != nil {
 		return err
@@ -26,7 +29,7 @@ func (usecase *GradeUseCase) PostGrade(grade *entities.Grade) error {
 }
 
 func (usecase *GradeUseCase) GetGradeByID(idGrade int) (*entities.Grade, error) {
-	if idGrade <= 0 {
+	if idGrade < 0 {
 		return &entities.Grade{}, fmt.Errorf("error invalid idGrade")
 	}
 	grade, err := usecase.GradeRepository.ReadGrade(idGrade)
@@ -39,6 +42,9 @@ func (usecase *GradeUseCase) GetGradeByID(idGrade int) (*entities.Grade, error) 
 func (usecase *GradeUseCase) PutGrade(idGrade int, grade *entities.Grade) error {
 	if idGrade <= 0 {
 		return fmt.Errorf("error invalid idGrade")
+	}
+	if grade.Grade < 0 || grade.Grade > 5 {
+		return fmt.Errorf("error invalid Grade")
 	}
 	err := usecase.GradeRepository.UpdateGrade(idGrade, grade)
 	if err != nil {
